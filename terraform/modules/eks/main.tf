ab9f8486d406fc788cb2abc23871245b3e15c282
zakariyaa4458 <zakariya.a4458@gmail.com>
2026-09-09 11:23:39 +0100

    increased desired size and min size for nodes

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -61,9 +61,9 @@ resource "aws_eks_node_group" "eks-node-group" {
   ]
 
   scaling_config {
-    desired_size = 1
+    desired_size = 2
     max_size     = 2
-    min_size     = 1
+    min_size     = 2
   }
 
   update_config {
