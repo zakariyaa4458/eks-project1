59eeaed259a7bb56b06378b2a443f45f73ce2833
zakariyaa4458 <zakariya.a4458@gmail.com>
2026-09-13 11:33:58 +0100

    added irsa for order service account

--- terraform/modules/irsa/main.tf
+++ terraform/modules/irsa/main.tf
@@ -173,3 +173,54 @@ resource "aws_iam_role_policy_attachment" "Worker_iam_policy_attachment" {
 
 
 
+resource "aws_iam_role" "order_iam_role" {
+  name = "order-iam-role"
+
+  assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+
+   Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+
+        Principal = {
+          Federated =  var.aws_iam_openid_connect_provider_arn
+        }
+
+        Condition = {
+          StringEquals = {
+        "oidc.eks.eu-west-2.amazonaws.com/id/507F56E12A4EA46AD10B6AA97B90F722:sub" = "system:serviceaccount:application-namespace:order-service-service-account"
+        "oidc.eks.eu-west-2.amazonaws.com/id/507F56E12A4EA46AD10B6AA97B90F722:aud" = "sts.amazonaws.com"               
+      }, 
+        }
+      }
+    ]
+
+  }
+  )
+}
+
+data "aws_iam_policy_document" "order_iam_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:SendMessage"
+    ]
+
+    
+    resources = ["arn:aws:sqs:eu-west-2:499024813019:eks_sqs"]
+  }
+
+}
+
+resource "aws_iam_policy" "order_iam_policy" {
+  name   = "order-iam-policy"
+  policy = data.aws_iam_policy_document.order_iam_policy_document
+}
+
+resource "aws_iam_role_policy_attachment" "order_iam_policy_attachment" {
+  role       = aws_iam_role.order_iam_role.name
+  policy_arn = aws_iam_policy.order_iam_policy.arn
+}
