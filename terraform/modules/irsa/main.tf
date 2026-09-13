d73429d3a31caf5270573f7a1acd0f3aeccd2c99
zakariyaa4458 <zakariya.a4458@gmail.com>
2026-09-13 11:02:26 +0100

    configured irsa for worker

--- terraform/modules/irsa/main.tf
+++ terraform/modules/irsa/main.tf
@@ -112,3 +112,64 @@ resource "aws_iam_role_policy_attachment" "AmazonEBSCSIDriverPolicy" {
   role       = aws_iam_role.ebs_csi_controller.name
   policy_arn =  "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
 }
+
+resource "aws_iam_role" "worker_iam_role" {
+  name = "worker-iam-role"
+
+  assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+
+   Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+
+        Principal = {
+          Federated =  var.aws_iam_openid_connect_provider_arn
+        }
+
+        Condition = {
+          StringEquals = {
+        "oidc.eks.eu-west-2.amazonaws.com/id/507F56E12A4EA46AD10B6AA97B90F722:sub" = "system:serviceaccount:application-namespace:worker-service-account"
+        "oidc.eks.eu-west-2.amazonaws.com/id/507F56E12A4EA46AD10B6AA97B90F722:aud" = "sts.amazonaws.com"               
+      }, 
+        }
+      }
+    ]
+
+  }
+  )
+}
+
+
+data "aws_iam_policy_document" "worker_iam_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:ReceiveMessage",
+      "sqs:DeleteMessage",
+      "sqs:GetQueueAttributes",
+      "sqs:GetQueueUrl",
+      "sqs:SendMessage"
+    ]
+
+    
+    resources = ["arn:aws:sqs:eu-west-2:499024813019:eks_sqs"]
+  }
+
+}
+
+resource "aws_iam_policy" "worker_iam_policy" {
+  name   = "worker-iam-policy"
+  policy = data.aws_iam_policy_document.worker_iam_policy_document.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "Worker_iam_policy_attachment" {
+  role       = aws_iam_role.worker_iam_role.name
+  policy_arn = aws_iam_policy.worker_iam_policy.arn
+}
+
+
+
