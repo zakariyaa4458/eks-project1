f6dc44568ff97295cff8dabaa537444afe36f010
zakariyaa4458 <zakariya.a4458@gmail.com>
2026-09-14 09:57:12 +0100

    created irsa for shipping service

--- terraform/modules/irsa/main.tf
+++ terraform/modules/irsa/main.tf
@@ -224,3 +224,55 @@ resource "aws_iam_role_policy_attachment" "order_iam_policy_attachment" {
   role       = aws_iam_role.order_iam_role.name
   policy_arn = aws_iam_policy.order_iam_policy.arn
 }
+
+resource "aws_iam_role" "shipping_iam_role" {
+  name = "shipping-iam-role"
+
+  assume_role_policy = jsonencode({
+   Version = "2012-10-17"
+
+   Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+
+        Principal = {
+          Federated =  var.aws_iam_openid_connect_provider_arn
+        }
+
+        Condition = {
+          StringEquals = {
+        "oidc.eks.eu-west-2.amazonaws.com/id/507F56E12A4EA46AD10B6AA97B90F722:sub" = "system:serviceaccount:application-namespace:shipping-service-service-account"
+        "oidc.eks.eu-west-2.amazonaws.com/id/507F56E12A4EA46AD10B6AA97B90F722:aud" = "sts.amazonaws.com"               
+      }, 
+        }
+      }
+    ]
+
+  }
+  )
+}
+
+data "aws_iam_policy_document" "shipping_iam_policy_document" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sqs:SendMessage"
+    ]
+
+    
+    resources = ["arn:aws:sqs:eu-west-2:499024813019:eks_sqs"]
+  }
+
+}
+
+resource "aws_iam_policy" "shipping_iam_policy" {
+  name   = "shipping-iam-policy"
+  policy = data.aws_iam_policy_document.shipping_iam_policy_document.json
+}
+
+resource "aws_iam_role_policy_attachment" "shipping_iam_policy_attachment" {
+  role       = aws_iam_role.shipping_iam_role.name
+  policy_arn = aws_iam_policy.shipping_iam_policy.arn
+}
