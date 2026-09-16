57ce46da4fa7b77450affdaa450578600541c118
zakariyaa4458 <zakariya.a4458@gmail.com>
2026-09-16 14:00:38 +0100

    increased scaling config

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -61,9 +61,9 @@ resource "aws_eks_node_group" "eks-node-group" {
   ]
 
   scaling_config {
-    desired_size = 3
-    max_size     = 3
-    min_size     = 3
+    desired_size = 4
+    max_size     = 4
+    min_size     = 4
   }
 
   update_config {
